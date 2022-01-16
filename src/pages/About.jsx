d7c22968f3315dc360abb8a53e9859d23cf5c8c6
ahmed-razic <ahmed.razic@gmail.com>
2022-01-16 22:27:59 +0100

    modified About page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,30 @@
 function About() {
   return (
     <>
-      <p className='card text-9xl font-mono'>ttttt</p>
-      <h1 className='text-6xl mb-4'>Solar System</h1>
+      <h1 className='text-6xl mb-4 text-white'>Our Solar System</h1>
       <p className='mb-4 text-2xl font-light'>
         A react app which provides detail information about planets, asteroids,
         comets and other bodies of our Solar system. This app is made by{' '}
-        <strong>
-          <a href='https://github.com/ahmed-razic'> Ahmed Razic</a>
+        <strong></strong>.
+      </p>
+      <p className='text-lg text-gray-400'>
+        APIs provided by{' '}
+        <strong className='text-white'>
+          <a
+            href='https://api.le-systeme-solaire.net/en/'
+            target='_blank'
+            rel='noreferrer'
+          >
+            {' '}
+            The Solar System OpenData
+          </a>
+        </strong>{' '}
+        <span className='text-lg text-gray-400'>and</span>{' '}
+        <strong className='text-white'>
+          <a href='https://api.nasa.gov/' target='_blank' rel='noreferrer'>
+            {' '}
+            NASA Open APIs
+          </a>
         </strong>
         .
       </p>
@@ -15,18 +32,17 @@ function About() {
         Version <span className='text-white'>1.0.0</span>
       </p>
       <p className='text-lg text-gray-400'>
-        APIs provided by{' '}
-        <strong>
-          <a href='https://api.le-systeme-solaire.net/en/'>
+        Author{' '}
+        <span className='text-white'>
+          <a
+            href='https://github.com/ahmed-razic'
+            target='_blank'
+            rel='noreferrer'
+          >
             {' '}
-            The Solar System OpenData
+            Ahmed Razic
           </a>
-        </strong>{' '}
-        and{' '}
-        <strong>
-          <a href='https://api.nasa.gov/'> NASA Open APIs</a>
-        </strong>
-        .
+        </span>
       </p>
     </>
   );
